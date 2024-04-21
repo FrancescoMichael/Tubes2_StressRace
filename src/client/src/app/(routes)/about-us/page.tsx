0d1f4e79f8ba51a65a75bd27cb5620edb765f203
dimasb <dimasbagoes1954@gmail.com>
2024-04-21 18:24:49 +0700

    update page

--- src/client/src/app/(routes)/about-us/page.tsx
+++ src/client/src/app/(routes)/about-us/page.tsx
@@ -2,7 +2,22 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="container-member flex min-h-screen flex-col items-center justify-between p-24 text-white">
+      <div className="top">
+        <p>36 | STRESSSSS</p>
+      </div>
+      <div className="member">
+        <p>13522038</p>
+        <p>Francesco Michael Kusuma</p>
+      </div>
+      <div className="member">
+        <p>13522075</p>
+        <p>Marvel Pangondian</p>
+      </div>
+      <div className="member">
+        <p>13522112</p>
+        <p>Dimas Bagoes Hendrianto</p>
+      </div>
 
     </main>
   );
--- src/client/src/app/(routes)/wiki-race/page.tsx
+++ src/client/src/app/(routes)/wiki-race/page.tsx
@@ -2,8 +2,15 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-
+    <main className="flex min-h-screen flex-col items-center p-24">
+      <div className="bg-white">
+        <img src="https://images.unsplash.com/photo-1566396223585-c8fbf7fa6b6d?q=80&w=1898&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D  " alt="TAI" className="h-64 w-full object-cover object-center"/>
+      </div>
+      <div className="text-white text-lg text-justify pt-20">
+        <p>
+        WikiRace atau Wiki Game adalah permainan yang melibatkan Wikipedia, sebuah ensiklopedia daring gratis yang dikelola oleh berbagai relawan di dunia, dimana pemain mulai pada suatu artikel Wikipedia dan harus menelusuri artikel-artikel lain pada Wikipedia (dengan mengeklik tautan di dalam setiap artikel) untuk menuju suatu artikel lain yang telah ditentukan sebelumnya dalam waktu paling singkat atau klik (artikel) paling sedikit.
+        </p>  
+      </div>  
     </main>
   );
 }
