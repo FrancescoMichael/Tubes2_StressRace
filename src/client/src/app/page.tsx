faf46ce8a799b49bbe0de3a60d1cc0766586cad4
dimasb <dimasbagoes1954@gmail.com>
2024-04-21 14:12:33 +0700

    Update page.tsx

--- src/client/src/app/page.tsx
+++ src/client/src/app/page.tsx
@@ -5,7 +5,7 @@ import Result from "@/components/Elements/Result/Result";
 export default function Home() {
     return (
         <main className="flex flex-col items-center justify-center p-12">
-            <div className="set flex flex-row items-center font-bold]">
+            <div className="judul flex flex-row items-center font-bold]">
                 <SearchIcon style={{ color: 'white', fontSize: '200px'}}/>
                 <h1 className="text-7xl text-white font-bold"> Wiki Game Solver </h1>
             </div>
