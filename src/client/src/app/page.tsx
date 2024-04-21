cb88d51abe3fbf9456b51f540614157ece53ad5d
dimasb <dimasbagoes1954@gmail.com>
2024-04-21 16:31:11 +0700

    Update page.tsx

--- src/client/src/app/page.tsx
+++ src/client/src/app/page.tsx
@@ -5,13 +5,11 @@ import Result from "@/components/Elements/Result/Result";
 export default function Home() {
     return (
         <main className="flex flex-col items-center justify-center p-12">
-            <div className="judul set flex flex-row items-center font-bold]">
-                <SearchIcon fontSize="inherit" style={{ color: 'white'}}/>
-                <h1 className="text-white font-bold"> Wiki Game Solver </h1>
+            <div className="judul set text-white flex flex-row items-center font-bold]">
+                <SearchIcon fontSize="inherit" color="inherit"/>
+                <h1 className="font-bold"> Wiki Game Solver </h1>
             </div>
-
             <InputForm/>
-
             <Result />
         </main>
     );
