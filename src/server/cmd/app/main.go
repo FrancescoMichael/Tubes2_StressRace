1224fac82ef823b439e790aa22edf04d95267dbe
Marvel Pangondian <marvel.pangondian01@gmail.com>
2024-04-26 12:27:31 +0700

    feat: refactor code

--- src/server/cmd/app/main.go
+++ src/server/cmd/app/main.go
@@ -1,50 +1,54 @@
-package main
-
-import (
-	"server/pkg/routers"
-
-	"github.com/gin-contrib/cors"
-	"github.com/gin-gonic/gin"
-)
-
-func main() {
-	router := gin.Default()
-
-	router.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"http://localhost:3000"},
-		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE"},
-		AllowHeaders:     []string{"Origin", "Content-Type"},
-		AllowCredentials: true,
-	}))
-
-	// Routes
-	router.POST("/api/search", routers.GetSearch)
-	router.GET("/api/result", routers.GetResult)
-	router.GET("/api/properties", routers.GetProperties)
-
-	// Run the server
-	router.Run(":8080")
-}
-
 // package main
 
 // import (
-// 	"log"
-// 	"os"
-// 	"os/signal"
-// 	"server/pkg/test"
-// 	"syscall"
+// 	"server/pkg/routers"
+
+// 	"github.com/gin-contrib/cors"
+// 	"github.com/gin-gonic/gin"
 // )
 
 // func main() {
-// 	sigs := make(chan os.Signal, 1)
-// 	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
+// 	router := gin.Default()
 
-// 	go func() {
-// 		sig := <-sigs
-// 		log.Printf("Received signal: %s", sig)
-// 		// Perform cleanup tasks, close resources, etc.
-// 		os.Exit(0) // Exit successfully after handling
-// 	}()
-// 	test.Test()
+// 	router.Use(cors.New(cors.Config{
+// 		AllowOrigins:     []string{"http://localhost:3000"},
+// 		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE"},
+// 		AllowHeaders:     []string{"Origin", "Content-Type"},
+// 		AllowCredentials: true,
+// 	}))
+
+// 	// Routes
+// 	router.POST("/api/search", routers.GetSearch)
+// 	router.GET("/api/result", routers.GetResult)
+// 	router.GET("/api/properties", routers.GetProperties)
+
+// 	// Run the server
+// 	router.Run(":8080")
 // }
+
+package main
+
+import (
+	"log"
+	"net/http"
+	_ "net/http/pprof"
+	"os"
+	"os/signal"
+	"server/pkg/test"
+	"syscall"
+)
+
+func main() {
+	sigs := make(chan os.Signal, 1)
+	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
+	go func() {
+		http.ListenAndServe("localhost:6060", nil)
+	}()
+	go func() {
+		sig := <-sigs
+		log.Printf("Received signal: %s", sig)
+		// Perform cleanup tasks, close resources, etc.
+		os.Exit(0) // Exit successfully after handling
+	}()
+	test.Test()
+}
